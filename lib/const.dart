45ada6dc0357587a1f6da4d1dbb69241e4d50267
Khairunnisa <khairunnyisa@gmail.com>
2025-03-13 13:56:19 +0700

    fixing const and make finishing onboarding screen

--- lib/const.dart
+++ lib/const.dart
@@ -1,43 +1,38 @@
 import 'package:flutter/material.dart';
 
 const primary100 = Color(0xFFD4DEF5);
-const primary200 = Color(0xFFB7CBEE);
-const primary300 = Color(0xFF87D5D5);
-const primary400 = Color(0xFF275CCD);
+const primary200 = Color(0xFFB7C8EE);
+const primary300 = Color(0xFF4B75D5);
+const primary400 = Color(0xFF2759CD);
 const primary500 = Color(0xFF214AAB);
-const primary600 = Color(0xFF1A8899);
+const primary600 = Color(0xFF1A3B59);
+const lightBlue = Color(0xFFF2F6FF);
 
 const secondary100 = Color(0xFFFDFDFD);
-const secondary200 = Color(0xFFF3F3F3);
-const secondary300 = Color(0xFFE6E6E6);
-const secondary400 = Color(0xFFDADADA);
-const secondary500 = Color(0xFFC6C6C6);
+const secondary200 = Color(0xFFF8F8F8);
+const secondary300 = Color(0xFFF3F3F3);
+const secondary400 = Color(0xFFEEEEEE);
+const secondary500 = Color(0xFFEBEBEB);
+const secondary600 = Color(0xFFE6E6E6);
 
 const warningLight100 = Color(0xFFFFF6CC);
-const warningLight200 = Color(0xFFFFE0AA);
-const warningLight300 = Color(0xFFFDD28B);
-const warningLight400 = Color(0xFFFDF101);
+const warningLight200 = Color(0xFFFFF0AA);
+const warningLight300 = Color(0xFFFFD92B);
+const warningLight400 = Color(0xFFFFD101);
 const warningLight500 = Color(0xFFD4AE01);
-const warningLight600 = Color(0xFFAA8801);
-
-const warning100 = Color(0xFFF2F6FF);
-const warning200 = Color(0xFFD9E8FF);
-const warning300 = Color(0xFFC8D9FF);
-const warning400 = Color(0xFFB0D1FF);
-const warning500 = Color(0xFF9DAEAD);
-const warning600 = Color(0xFF7E8BAA);
+const warningLight600 = Color(0xFFAA8B01);
 
 const success100 = Color(0xFFD6F7DA);
-const success200 = Color(0xFF88F2C1);
-const success300 = Color(0xFF50DE63);
+const success200 = Color(0xFFBBF2C1);
+const success300 = Color(0xFF56DE63);
 const success400 = Color(0xFF34D844);
 const success500 = Color(0xFF28B439);
-const success600 = Color(0xFF239D02);
+const success600 = Color(0xFF23902D);
 
 const error100 = Color(0xFFFFCCCC);
 const error200 = Color(0xFFFFAAAA);
-const error300 = Color(0xFFFE6E56);
-const error400 = Color(0xFFFF0D00);
+const error300 = Color(0xFFFF5555);
+const error400 = Color(0xFFFF0000);
 const error500 = Color(0xFFD40000);
 const error600 = Color(0xFFAA0000);
 
@@ -49,3 +44,14 @@ const text500 = Color(0xFF777777);
 const text600 = Color(0xFF555555);
 
 const defaultPadding = 20.0; 
+
+const double heading1 = 24;
+const double heading2 = 20;
+const double heading3 = 18;
+
+const double descText = 16;
+const double body= 14;
+const double caption = 12;
+
+const animationDuration = Duration(milliseconds: 200);
+
