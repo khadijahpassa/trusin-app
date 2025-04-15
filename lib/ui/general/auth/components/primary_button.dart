663d1ecd4dbd5d2b240d8c6d7c346a1d904da065
Odit <khadijahpassa@gmail.com>
2025-04-15 09:29:18 +0700

    completing auth feature, create dashboard_superadmin.dart, add several dependencies

--- lib/ui/general/auth/components/primary_button.dart
+++ lib/ui/general/auth/components/primary_button.dart
@@ -20,25 +20,15 @@ class PrimaryButton extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         ),
-<<<<<<< HEAD
         minimumSize: const Size(double.infinity, 40), 
         padding: const EdgeInsets.symmetric(vertical: 12), 
-=======
-        minimumSize: const Size(double.infinity, 50), 
-        padding: const EdgeInsets.symmetric(vertical: 16), 
->>>>>>> 74037b170cd5f17f7b350e01213ede762cc5d454
       ),
       child: Text(
         text, 
         style: TextStyle(
           color: Colors.white,
-<<<<<<< HEAD
           fontWeight: FontWeight.w600,
           fontSize: descText,
-=======
-          fontWeight: FontWeight.w500,
-          fontSize: heading3,
->>>>>>> 74037b170cd5f17f7b350e01213ede762cc5d454
           )
         ),
     );
